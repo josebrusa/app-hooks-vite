1b56b6f08f63dfc8034cabaf730aee31e28c91a7
josebrusa <josebrusa@outlook.com>
2023-08-31 01:36:50 +0200

    useContext finish

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,10 +3,11 @@ import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
 import { NavBar } from "./NavBar";
+import { UserProvider } from "./context/UserProvider";
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <NavBar />
             <hr />
             <Routes>
@@ -15,6 +16,6 @@ export const MainApp = () => {
                 <Route path="login" element={<LoginPage />} />
                 <Route path="/*" element={<Navigate to="/about" />} />
             </Routes>
-        </>
+        </UserProvider>
     );
 };
---
+++ src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { UserContext } from "./UserContext";
+
+const user = {
+    id: "123",
+    name: "jose",
+    email: "jose@test.com",
+};
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
